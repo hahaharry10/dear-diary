1531c1caff3bf41bc260d0dc980b2ee5933376ab
hahaharry10 <hnixon@hahaharry.dev>
2024-10-17 02:02:21 +0100

    WIP: start creating first test.
    TODO: fix bug that leaves input into command line.

--- testDearDiary.sh
+++ testDearDiary.sh
@@ -1,19 +1,32 @@
 #!/bin/bash
 
 mkdir test/
-cp ./dear-diary.sh
+cp ./dear-diary.sh test/
 cd test/
 
-for fileNum in {0..5}; do
-    echo "This is File number $fileNum" > "File$fileNum.drd"
-done
+GREEN="\e[1;32m"
+RED="\e[1;31m"
+RESET="\e[0m"
 
 ######################## Test MVP: ##########################
 # NOTE: Tests...
 #   1) File not in directory. and nothing saved (file is empty).
 #   2) File not in directory. And text is inserted and saved. Password is created. Password is confirmed successfully. Encryption is tested.
 #   3) File not in directory. And text is inserted and saved. Password is created. Password fails confirmation. Password is then created and confirmed correctly. Encryption is tested.
-spawn ./dear-diary.sh
+
+errCode=$(./test1 test.txt)
+if [[ $errCode = 0 ]] 
+then
+    echo -e "Test 1: ${GREEN}PASSED${RESET}\n"
+else
+    echo -e "Test 1: ${RED}FAILED${RESET} ($errCode). "
+    if [[ $errCode = 1 ]]
+    then
+        echo -e "Invalid usage of test1.\n"
+    else
+        echo -e "Unknown Error\n"
+    fi
+fi
 
 cd ../
 rm -r test/
