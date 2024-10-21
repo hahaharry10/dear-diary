8f5d2ef3ba69752288c562c149384c31a75daa1f
hahaharry10 <hnixon@hahaharry.dev>
2024-10-21 19:12:02 +0100

    Change error code 3 to be more vague.

--- testDearDiary.sh
+++ testDearDiary.sh
@@ -40,9 +40,11 @@ fi
 mkdir test/
 cp ./dear-diary.sh test/
 cp ./test1.exp test/test1.exp
+cp ./test2.exp test/test2.exp
 cd test/
 
 failNum=0 # Track number of tests that have failed.
+FILE="test.txt"
 
 ######################## Test MVP: ##########################
 # NOTE: Tests...
@@ -56,12 +58,10 @@ failNum=0 # Track number of tests that have failed.
 #       - The file does not get created in the end (file does not exist before or after execution).
 #
 errNum=0
-FILE="test.txt"
 ./test1.exp $FILE 1>/dev/null
 errCode=$?
 if [[ $errCode != 0 ]] 
 then
-    errMessage="Test 1: ${RED}ERROR${RESET} ($errCode):"
     errMessage=$(getErrMessage "Test 1: ${RED}ERROR${RESET} ($errCode):" 1 $errCode)
     ((++errNum))
     echo -e $errMessage
@@ -81,6 +81,44 @@ else
     ((++failNum))
     echo -e "Test 1: ${RED}FAILED${RESET} $errNum tests."
 fi
+
+
+# TEST: 2:
+#   Requirements for passing:
+#       - expect script spawns and interacts with dear-diary successfully.
+#           1) File is saved.
+#           2) Password entered.
+#           3) Password re-entered correctly.
+#       - File persists in directory with following standard $FILE.dd (e.g. if file is 'foo.txt' the resulting file is 'foo.txt.dd')
+#       - Contents of the file do not match original message.
+if [[ -f $FILE ]]
+then
+    rm $FILE
+fi
+errNum=0
+./test2.exp $FILE 1>/dev/null
+errCode=$?
+if [[ $errCode != 0 ]] 
+then
+    errMessage=$(getErrMessage "Test 2: ${RED}ERROR${RESET} ($errCode):" 2 $errCode)
+    ((++errNum))
+    echo -e $errMessage
+fi
+
+if [[ ! -f $FILE ]]
+then
+    ((++errNum))
+    echo -e "Test 2: ${RED}ERROR${RESET} File should persist in directory."
+fi
+
+if [[ errNum = 0 ]]
+then
+    echo -e "Test 2: ${GREEN}PASSED${RESET}"
+else
+    ((++failNum))
+    echo -e "Test 2: ${RED}FAILED${RESET} $errNum tests."
+fi
+
 
 cd ../
 rm -r test/
